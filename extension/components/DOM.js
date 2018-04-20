dc57403159d0ea3be03f6bbd843ca14b175e82a8
Connor Deckers <Connor.Deckers@utas.edu.au>
2018-04-20 16:42:55 +1000

    Adds some documentation in the comments.

--- extension/components/DOM.js
+++ extension/components/DOM.js
@@ -1,37 +1,49 @@
 import { guid } from './utils/Utils.js';
 
+// This structure provides a transparent layer to access the DOMs variables.
+//	Still needs doing: find a way to serialize and transmit DOM nodes. Hmm.
 export class DOMChannel {
 	constructor() {
+		// Create a unique, custom GUID for this communications channel.
+		//	This is better than using Date.now, as it means multiple calls can be done in the same millisecond and still have unique ID's.
 		this.ChannelID = guid();
+
+		// Listen for when our other half, 'DOMMessageHandler.js' is loaded and ready, and connects back to us.
 		chrome.runtime.onConnectExternal.addListener(port => {
-			this.port = port;
-			let self = this;
+			this.port = port; let self = this;
+			
+			// Wait until the DOM is ready
 			this.post({req: 'ready'})
-				.then(() => this.post({ req: 'vars' }))
+				// Build a proxy based on these elements.
 				.then(vars => this.doc = new Proxy({}, {
-					has: function (target, key) { return vars.includes(prop) },
-					get: async function(target, prop) {
-						if (vars.includes(prop)) { return await self.post({req: 'get_var', prop }); }
-						else return undefined;
+					// Check against the window object to see if the variable exists.
+					has: function (target, key) { 
+						let vars = await this.post({ req: 'vars' });
+						return vars.includes(prop);
 					},
+					// Get the variable directly from the DOM
+					get: async function(target, prop) {
+						return await self.post({req: 'get_var', prop });
+					},
+					// Set the variable on the DOM
 					set: function(obj, prop, value) { self.post({req: 'set_var', prop, value }); }
 				}));
 		});
-		console.log(this);
 	}
 
-	on(event, cb) {
-		this.post({req: 'event', name: event}).then(cb);
+	// Set an event handler to occur on the document.
+	//	This might get fleshed out later to allow it to attach to live nodes (checks nodes event at time of event occuring).
+	on(event, cb, once) {
+		this.post({req: 'event', name: event, once}).then(cb);
 	}
 
+	// Posts a packet to the DOM and waits for it to get a direct response (based on Channel and PacketID).
+	// Once it receives a response, it will return a Resolved Promise, which can be captured with a .then() or an await.
 	post(packet) {
 		return new Promise((resolve, reject) => { 
 			let id = guid();
 			const handler = (msg) => { 
-				if (msg.channel == this.ChannelID && msg.id == id) { 
-					resolve(msg.data); 
-					this.port.onMessage.removeListener(handler);
-				} 
+				if (msg.channel == this.ChannelID && msg.id == id) { resolve(msg.data); this.port.onMessage.removeListener(handler); } 
 			}
 			this.port.onMessage.addListener(handler);
 			this.port.postMessage({ channel: this.ChannelID, data: packet, id });
--- extension/components/DOMMessageHandler.js
+++ extension/components/DOMMessageHandler.js
@@ -1,38 +1,46 @@
+// This class runs directly in the DOM; it is added whenever there is at least one Content script running in a page.
 class DOMMessageHandler {
 	constructor(extensionID) {
+		// Connect to the background DOM.js message handler.
 		this.port = chrome.runtime.connect(extensionID);
+		
+		// Listen for any packet changes.
 		this.port.onMessage.addListener(this.receive.bind(this));
 	}
 
+	// Wrapper to send a message back to the content script who sent the request.
 	send(channel, packet, id) {
 		this.port.postMessage({ channel, data: packet, id});
 	}
 
+	// Listens for an incoming packet.
 	receive(packet) {
+		// Process the request provided
 		switch(packet.data.req) {
+			// Return all the variables on the window object
 			case 'vars':
 				this.send(packet.channel, Object.keys(window), packet.id);
 				break;
 
+			// Attach an event to the document object
 			case 'event':
-				document.addEventListener(packet.data.name, e => {
-					this.send(packet.channel, e, packet.id);
-				});
+				const handler = e => { this.send(packet.channel, e, packet.id); if(packet.data.once) document.removeEventListener(packet.data.name, handler); }
+				document.addEventListener(packet.data.name, handler);
 				break;
 
+			// Send a packet back when the document is ready.
+			//	If the document is already ready when the request is received, send a response back immediately.
 			case 'ready': 
 				if (document.readyState == 'complete') this.send(packet.channel, null, packet.id);
-				else {
-					document.addEventListener('load', e => {
-						this.send(packet.channel, null, packet.id);
-					});
-				}
+				else { document.addEventListener('load', e => { this.send(packet.channel, null, packet.id); }); }
 				break;
 
+			// Used to retrieve a specific variable from the DOM.
 			case 'get_var': 
 				this.send(packet.channel, window[packet.data.prop], packet.id);
 				break;
 
+			// Used to update a specific variable from the DOM.
 			case 'set_var': 
 				window[packet.data.prop] = packet.data.value;
 				this.send(packet.channel, null, packet.id);
@@ -41,5 +49,7 @@ class DOMMessageHandler {
 	}
 }
 
+// Get the ExtensionID from the querystring attached to the source of this script.
 let eid = document.querySelector('script[src*="eid="]').src.split('eid=')[1].split('&')[0];
+// Start up the message handler.
 new DOMMessageHandler(eid);
